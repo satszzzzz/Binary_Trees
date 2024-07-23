f209a808b3ea4af1d5761778d13b20b63e7c1e9c
Satwik Das <satwikdas872@gmail.com>
2024-07-23 23:26:16 +0530

    Ninth version main

--- generate.py
+++ generate.py
@@ -1,4 +1,4 @@
-for i in range(10):
+for i in range(11):
     f=open(f'BT_{i+11}.java','w')
     lines=['import java.util.*;\n',f'public class BT_{i+11}\n','{\n   public static void main(String args[])\n    {\n','        \n    }\n}']
     f.writelines(lines)
